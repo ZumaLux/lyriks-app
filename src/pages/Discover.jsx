534ec2fe1e2993e568000c955d254dd5cca8e43f
ZumaLux <zumalux99@gmail.com>
2023-06-21 18:40:46 +0100

    Searchbar styled, genre filter wont work because the api does not support search by genre

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,24 +3,28 @@ import { genres } from "../assets/constants";
 
 import { useGetTopTracksQuery } from "../redux/services/spotify";
 import { useDispatch, useSelector } from "react-redux";
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () => {
   const { data, isFetching, error } = useGetTopTracksQuery();
-  const genreTitle = "Pop";
 
   const dispatch = useDispatch();
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    (state) => state.player
+  );
 
   if (isFetching) return <Loader title="Loading songs..." />;
   if (error) return <Error />;
+
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white">Discover {genreTitle}</h2>
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "pop"}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
